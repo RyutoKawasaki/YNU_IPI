c8469fc36153532345fd7e4628a821c78f56f4a6
Ryuton <>
2018-04-13 16:28:14 +0900

    第1回

---
+++ 01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+  // 練習問題 1
+  printf("Hello, YNU!\n");
+
+  // 練習問題 2
+  int a = 10;
+  a = a * a;
+  printf("a = %d\n", a);
+
+  //  練習問題 3
+  // 問題 1
+  // char, int, float, double
+
+  // 問題 2
+  int b = 5;
+  printf("value = %d\n", b + b + b);
+  // value = 15
+
+  // 問題 3
+  // 変数 a に a + b という値を代入
+
+  // 問題 4
+  // i++;
+
+  return 0;
+}
